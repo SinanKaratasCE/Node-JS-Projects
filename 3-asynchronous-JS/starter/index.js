ca1ae4cf3087443f4dac88cea1d7f31e4326b4d4
SinanKaratas <sinan.karatas.bm@gmail.com>
2024-06-19 01:40:48 +0300

    gitignore added

--- 3-asynchronous-JS/starter/index.js
+++ 3-asynchronous-JS/starter/index.js
@@ -1,6 +1,38 @@
 const fs = require(`fs`);
-const http = require(`http`);
+const superagent = require(`superagent`);
 
-fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
-  console.log(`Breed: ${data}`);
-});
+const readFilePro = (file) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      if (err) reject(`I could not find that file :(`);
+      resolve(data);
+    });
+  });
+};
+
+const writeFilePro = (file, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, data, (err) => {
+      if (er) reject(`Could  not write the file :(`);
+      resolve(`success`);
+    });
+  });
+};
+
+readFilePro(`${__dirname}/dog.txt`)
+  .then((data) => {
+    console.log(`Breed: ${data}`);
+
+    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+  })
+  .then((res) => {
+    console.log(res.body.message);
+
+    fs.writeFile(`dog-img.txt`, res.body.message, (err) => {
+      if (err) return console.log(err.message);
+      console.log(`Random dog image saved to file!`);
+    });
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
