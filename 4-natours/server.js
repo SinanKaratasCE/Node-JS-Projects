4b282612ce307a3ea5b2c21973e616d4c1e34e90
SinanKaratas <130935850+SinanKaratasCE@users.noreply.github.com>
2024-06-27 20:31:09 +0300

    office update

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -1,7 +1,19 @@
 const dotenv = require(`dotenv`);
+const mongoose = require(`mongoose`);
 const app = require(`./app`);
 
 dotenv.config({ path: `./config.env` });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
+await mongoose.connect(DB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: false,
+});
 
 //console.log(process.env);
 
