d971ae05f745a31218da905e5f62f1d637a267f1
SinanKaratas <sinan.karatas.bm@gmail.com>
2024-07-08 22:18:20 +0300

    merge

--- 4-natours/controllers/tourController.js
+++ 4-natours/controllers/tourController.js
@@ -1,10 +1,6 @@
 const fs = require(`fs`);
 const Tour = require(`./../models/tourModel`);
 
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`),
-);
-
 exports.checkID = (req, res, next, val) => {
   console.log(`Tour id is ${val}`);
   if (req.params.id * 1 > tours.length) {
@@ -34,32 +30,14 @@ exports.gettAllTours = (req, res) => {
   res.status(200).json({
     status: `success`,
     resquestedAt: req.requestTime,
-    results: tours.length,
+    /* results: tours.length,
     data: {
       tours,
-    },
+    }, */
   });
 };
 
-exports.createNewTour = (req, res) => {
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-
-  tours.push(newTour);
-
-  fs.writeFile(
-    '${__dirname}/dev-data/data/tours-simple.json',
-    JSON.stringify(tours),
-    (err) => {
-      res.status(201).json({
-        status: `success`,
-        data: {
-          tour: newTour,
-        },
-      });
-    },
-  );
-};
+exports.createNewTour = (req, res) => {};
 
 exports.getTour = (req, res) => {
   res.status(200).json({
