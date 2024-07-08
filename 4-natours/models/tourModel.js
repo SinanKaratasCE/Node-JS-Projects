94983237974ee9a5d1627b533c40f609ca58c444
SinanKaratas <130935850+SinanKaratasCE@users.noreply.github.com>
2024-07-08 19:10:55 +0300

    mongodb middlewares

--- 4-natours/models/tourModel.js
+++ 4-natours/models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require(`mongoose`);
+const slugify = require(`slugify`);
 
 const tourSchema = new mongoose.Schema(
   {
@@ -8,6 +9,7 @@ const tourSchema = new mongoose.Schema(
       unique: true,
       trim: true,
     },
+    slug: String,
     duration: {
       type: Number,
       required: [true, `A tour must have a duration`],
@@ -53,6 +55,10 @@ const tourSchema = new mongoose.Schema(
       select: false,
     },
     startDates: [Date],
+    scretTour: {
+      type: Boolean,
+      default: false,
+    },
   },
   {
     toJSON: {
@@ -60,12 +66,44 @@ const tourSchema = new mongoose.Schema(
     },
     toObject: {
       virtuals: true,
-    }
+    },
   },
 );
 
 tourSchema.virtual(`durationWeeks`).get(function () {
   return this.duration / 7;
+});
+
+// DOCUMENT MIDDLEWARE: runs before.save() and.create()
+tourSchema.pre(`save`, function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// tourSchema.pre(`save`, (next) => {
+//   console.log(`Will save document`);
+//   next();
+// });
+
+// tourSchema.post(`save`, (doc, next) => {
+//   console.log(doc);
+//   next();
+// });
+
+//QUERY MIDDLEWARE: runs after.()save() and.create()
+
+// /^find/ = this usage gets all the query starts with find
+tourSchema.pre(/^find/, function (next) {
+  this.find({ secretTour: { $ne: true } });
+
+  this.start = Date.now();
+  next();
+});
+
+tourSchema.post(/^find/, (docs, next) => {
+  console.log(`Query took ${Date.now() - this.start} miliseconds`);
+  console.log(docs);
+  next();
 });
 
 const Tour = mongoose.model(`Tour`, tourSchema);
